7f6df2966ea8d04da373ec6fcb139a0940c7dbb6
=Sherwin <9940511@qq.com>
2021-01-02 15:53:33 +0800

    master

--- node原生服务器/index.js
+++ node原生服务器/index.js
@@ -1,7 +1,12 @@
-let http = require('http')
+let http = require('http');
+let qs =require('querystring');
 let server = http.createServer(function(req,res){
     res.setHeader('content-type','text/html;charset=utf-8')
-    res.end('<h1>水水水水水水水水水水水水水水水水</h1>');
+    let params = req.url.split('?')[1];
+    console.log(params);
+    params=qs.parse(params)
+    console.log(params);
+    res.end(params.name+'----'+params.age);
 })
 server.listen(4000,function(err){
     if(!err){
