1768cc26af28952ef2bbf06f6eccd3a970235789
=Sherwin <9940511@qq.com>
2021-01-03 10:27:52 +0800

    test

--- express/app.js
+++ express/app.js
@@ -3,6 +3,13 @@ const router = express();
 router.get('/',(req,res)=>{
     res.send(req.query);
 })
+
+
+router.get('/:id',(req,res)=>{
+    // res.download('express/package.json')
+    res.sendFile('E:/cangku/node/express/package.json')
+    // res.send(req.params)
+})
 router.listen(3000,(err)=>{
     !err?console.log('ok'):console.log('error');
 })
